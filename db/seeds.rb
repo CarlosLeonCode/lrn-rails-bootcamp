7f6e431252047f05f4da321c0f9e3e3ff487f02e
Carlos León <carlosestebanleonpinilla@gmail.com>
2022-01-06 14:36:52 -0500

    portfolio index added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+    Blog.create!(
+        title:      "My blog post #{i + 1}",
+        body:       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+    )
+end
+puts "10 Blogs created"
+
+5.times do |i|
+    Skill.create!(
+        title:      "Rails #{i + 1}"
+    )
+end
+puts "5 Skills created"
+
+
+9.times do |i|
+    Portfolio.create!(
+        title:      "Portfolio #{i + 1}",
+        subtitle:   "Topic #{ i + 1 }",
+        body:       "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+        main_image: "https://via.placeholder.com/600x400",
+        thumb_image:"https://via.placeholder.com/350x200"
+    )
+end
+puts "9 Portfolios created"
