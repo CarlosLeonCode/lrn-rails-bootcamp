267b401328ea62403b73f4917abbe6742ad3c95e
Carlos León <carlosestebanleonpinilla@gmail.com>
2022-01-07 10:14:21 -0500

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  resources :blogs
   get 'about',    to: 'pages#about'
   get 'contact',  to: 'pages#contact'
   root 'pages#home'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  resources :blogs
 end
