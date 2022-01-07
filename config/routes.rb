a82decb4d73e218389bcfce484b14b26fe3e7706
Carlos León <carlosestebanleonpinilla@gmail.com>
2022-01-07 09:35:46 -0500

    routes customized

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios
   resources :blogs
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-
-  
-
+  get 'about',    to: 'pages#about'
+  get 'contact',  to: 'pages#contact'
+  root 'pages#home'
 end
