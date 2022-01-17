eec6862fb2021ceef9519cf89ab0c0f36b78e434
Carlos León <carlosestebanleonpinilla@gmail.com>
2022-01-17 14:15:07 -0500

    defaults to img on portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,11 @@ class Portfolio < ApplicationRecord
 
     scope :ruby_on_rails, -> { where("title LIKE '%Ruby on Rails%'") }
     scope :react, -> { where("title LIKE '%React%'") }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image     ||= "https://via.placeholder.com/600x400"
+        self.thumb_image    ||= "https://via.placeholder.com/350x200"
+    end
 end
